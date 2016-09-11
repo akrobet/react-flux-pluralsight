f0e9bbe1b18526a337e695889abfa8ef35444faf
Roland Beta <betaroland@gmail.com>
2016-09-11 17:21:50 +0200

    Setup gulp tasks, create react main, home, about components, simple routing

---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+"use strict";
+
+var gulp = require('gulp');
+var connect = require('gulp-connect'); //runs a local dev server
+var open = require('gulp-open'); //open a URL in a web browser
+var browserify = require('browserify'); //bundles JS
+var reactify = require('reactify'); //transforms React JSX to JS
+var source = require('vinyl-source-stream'); //use conventional text streams with Gulp
+var concat = require('gulp-concat'); //concatenates files
+var lint = require('gulp-eslint'); //lint JS files, including JSX
+
+var config = {
+	port: 9005,
+	devBaseUrl: 'http://localhost',
+	paths: {
+		html: './src/*.html',
+		js: './src/**/*.js',
+		images: './src/images/*',
+		css: [
+			'node_modules/bootstrap/dist/css/bootstrap.min.css',
+			'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+		],
+		dist: './dist',
+		mainJs: './src/main.js'
+	}
+};
+
+//start a local development server
+gulp.task('connect', function() {
+	connect.server({
+		root: ['dist'],
+		port: config.port,
+		base: config.devBaseUrl,
+		livereload: true
+	});
+});
+
+//get the given html file and open it in the browser at the url
+gulp.task('open', ['connect'], function() {
+	gulp.src('dist/index.html')
+		.pipe(open({ uri: config.devBaseUrl + ':' + config.port + '/#'}))
+});
+
+//copy any html files from src to dist and reload it in the dev server
+gulp.task('html', function() {
+	gulp.src(config.paths.html)
+		.pipe(gulp.dest(config.paths.dist))
+		.pipe(connect.reload());
+});
+
+//transform js files with reactify plugin and bundle into one; place it at dest path and reload the page
+gulp.task('js', function() {
+	browserify(config.paths.mainJs)
+		.transform(reactify)
+		.bundle()
+		.on('error', console.error.bind(console))
+		.pipe(source('bundle.js'))
+		.pipe(gulp.dest(config.paths.dist + '/scripts'))
+		.pipe(connect.reload());
+});
+
+//concatenate css files at src, concatenate them, drop at destination
+gulp.task('css', function(){
+	gulp.src(config.paths.css)
+		.pipe(concat('bundle.css'))
+		.pipe(gulp.dest(config.paths.dist +  '/css'));
+});
+
+//migrates images to dist folder
+//note that I could even optimize my images here
+gulp.task('images', function () {
+	gulp.src(config.paths.images)
+		.pipe(gulp.dest(config.paths.dist + '/images'))
+		.pipe(connect.reload());
+
+	//publish favicon
+	gulp.src('./src/favicon.ico')
+		.pipe(gulp.dest(config.paths.dist));
+});
+
+gulp.task('lint', function(){
+	return gulp.src(config.paths.js)
+		.pipe(lint({config: 'eslint.config.json'}))
+		.pipe(lint.format());
+});
+
+//reload the browser every time a file is changed
+gulp.task('watch', function() {
+	gulp.watch(config.paths.html, ['html']);
+	gulp.watch(config.paths.js, ['js', 'lint']);
+});
+
+//single default task to run that includes all other dependent tasks
+gulp.task('default', ['html', 'js', 'css', 'images', 'lint', 'open', 'watch']);
