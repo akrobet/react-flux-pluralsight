d2bc3d6d8a7b94a82297788897d847da48e46299
Roland Beta <betaroland@gmail.com>
2016-09-12 21:40:48 +0200

    Added input form, toast notification, validation

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -3,6 +3,8 @@
 var React = require('react');
 var AuthorApi = require('../../api/authorApi');
 var AuthorList = require('./authorList');
+var Router = require('react-router');
+var Link = require('react-router').Link;
 
 var AuthorPage = React.createClass({
 
@@ -24,6 +26,7 @@ var AuthorPage = React.createClass({
         return (
             <div>
                 <h1>Authors</h1>
+                <Link to="addAuthor" className="btn btn-default">Add Author</Link>
                 <AuthorList authors={this.state.authors} />
             </div>
         );
--- src/routes.js
+++ src/routes.js
@@ -12,7 +12,10 @@ var routes = (
   <Route name="app" path="/" handler={require('./components/app')}>
     <DefaultRoute handler={require('./components/homePage')} />
     <Route name="authors" handler={require('./components/authors/authorPage')} />
+    <Route name="addAuthor" path="author" handler={require('./components/authors/manageAuthorPage')} />
+    <Route name="manageAuthor" path="author/:id" handler={require('./components/authors/manageAuthorPage')} />
     <Route name="about" handler={require('./components/about/aboutPage')} />
+    <Route name="authors/" handler={require('./components/about/aboutPage')} />
     <NotFoundRoute handler={require('./components/NotFoundRoute')} />
     <Redirect from="about-us" to="about" /> //redirect for retired urls
     <Redirect from="awthurs" to="authors" /> //redirect for typos
